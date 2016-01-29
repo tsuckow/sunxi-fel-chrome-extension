2f9000048590b22ee05238ed5276c9c97aba7d14
Thomas Suckow <tsuckow@gmail.com>
2016-01-28 17:47:01 -0800

    Be compatible with Windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
+var path = require('path');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  context: __dirname + '/src',
+  context: path.join(__dirname,'src'),
   entry: {
     main:'./js/main.js',
     ui:'./js/ui.js',
   },
   resolve: {
     fallback: [
-      __dirname + '/src',
+      path.join(__dirname,'src'),
     ],
   },
   output: {
-    path: __dirname + '/extension',
+    path: path.join(__dirname,'extension'),
     filename: '[name].js'
   },
   module: {
     loaders: [
       {
         test: /.jsx?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react']
